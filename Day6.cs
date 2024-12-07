829cc7bf10b9a6819530fdb72dcae9e771bfb709
maxime <maxime.challier@gmail.com>
2024-12-07 23:46:20 +0100

    day 6 part 1

---
+++ Day6.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC_2024;
+
+
+internal class Guard(int posX, int posY, int direction)
+{
+    public int PosX { get; private set; } = posX;
+    public int PosY { get; private set; } = posY;
+
+    /*
+     * Directions in clockwise turn (like default rotation): 0 = up, 1 = right, 2 = down, 3 = left
+     */
+    public int Direction { get; private set; } = direction;
+
+    static readonly Dictionary<int, int[]> directionToMove = new()
+    {
+        { 0, [-1, 0]},
+        { 1, [0, 1]},
+        { 2, [1, 0]},
+        { 3, [0, -1]},
+    };
+
+    internal bool OneStepForwardIsOnScreen(int[][] room)
+    {
+        int[] move = directionToMove[Direction];
+        int movedX = PosX + move[0];
+        int movedY = PosY + move[1];
+        return movedX >= 0 && movedX < room.Length
+            && movedY >= 0 && movedY < room[movedX].Length;
+    }
+
+    internal HashSet<Tuple<int, int>> MoveUntilObstacleOrEndOfScreen(int[][] room)
+    {
+        HashSet<Tuple<int, int>> positions = [];
+        int[] move = directionToMove[Direction];
+        while (OneStepForwardIsOnScreen(room) && room[PosX + move[0]][PosY + move[1]] == 0)
+        {
+            PosX += move[0];
+            PosY += move[1];
+
+            positions.Add(new(PosX, PosY));
+        }
+        return positions;
+    }
+
+    internal void Rotate()
+    {
+        this.Direction = (Direction + 1) % 4;
+    }
+}
+
+internal class InputDay6(Guard guard, int[][] room)
+{
+    public Guard Guard { get; } = guard;
+    public int[][] Room { get; } = room;
+
+
+    internal static InputDay6 Parse(string pathToFile)
+    {
+        string[] lines = File.ReadAllLines(pathToFile);
+        Guard guard = null;
+        int[][] room = new int[lines.Length][];
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            room[i] = new int[line.Length];
+
+            for (int j = 0; j < line.Length; j++)
+            {
+                char character = line[j];
+                if (character == '.')
+                {
+                    room[i][j] = 0;
+                }
+                else if (character == '#')
+                {
+                    room[i][j] = 1;
+                }
+                else if (character == '^')
+                {
+                    guard = new Guard(i, j, 0);
+                }
+                else if (character == '>')
+                {
+                    guard = new Guard(i, j, 1);
+                }
+                else if (character == 'v')
+                {
+                    guard = new Guard(i, j, 2);
+                }
+                else if (character == '<')
+                {
+                    guard = new Guard(i, j, 2);
+                }
+            }
+        }
+        return new InputDay6(guard, room);
+    }
+}
+
+internal class Day6Solver
+{
+    internal int SolvePart1(InputDay6 input)
+    {
+        Guard guard = input.Guard;
+        HashSet<Tuple<int, int>> distinctPositions = [new(guard.PosX, guard.PosY)];
+        int[][] room = input.Room;
+
+        bool touchedEdgeOfScreen = false;
+        while (!touchedEdgeOfScreen)
+        {
+            var newPositions = guard.MoveUntilObstacleOrEndOfScreen(room);
+            foreach (var pos in newPositions)
+            {
+                distinctPositions.Add(pos);
+            }
+            touchedEdgeOfScreen = !guard.OneStepForwardIsOnScreen(room);
+            guard.Rotate();
+        }
+        return distinctPositions.Count;
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -10,14 +10,15 @@ namespace AOC_2024
     {
         public static void Main(string[] args)
         {
-            string path = "../../inputs/input_day_5.txt";
-            InputDay5 input = InputDay5.Parse(path);
+            string path = "../../inputs/input_day_6.txt";
+            InputDay6 input = InputDay6.Parse(path);
 
-            Day5Solver day5Solver = new();
-            int solutionPart1 = day5Solver.SolvePart1(input);
+            Day6Solver solver = new();
+            int solutionPart1 = solver.SolvePart1(input);
             Console.WriteLine(solutionPart1);
-            int solutionPart2 = day5Solver.SolvePart2(input);
-            Console.WriteLine(solutionPart2);
+
+            //int solutionPart2 = day5Solver.SolvePart2(input);
+            //Console.WriteLine(solutionPart2);
 
             Console.ReadLine();
         }
